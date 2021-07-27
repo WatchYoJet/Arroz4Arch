7c6672610c3f7be1225f98af410b481be4441a9a
WatchYoJet <pedropereira972@gmail.com>
2021-07-27 13:38:03 +0100

    Revert "Fix Icons"

--- dwm/config.h
+++ dwm/config.h
@@ -3,46 +3,47 @@
 
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int gappx		= 5;
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const int focusonwheel       = 0;
+static const int focusonwheel       = 1;
 static const int horizpadbar        = 0;        /* horizontal padding for statusbar */
-static const int vertpadbar         = 7;        /* vertical padding for statusbar */
-static const char *fonts[]          = {"JetBrainsMono Nerd Font:size15"};
+static const int vertpadbar         = 5;        /* vertical padding for statusbar */
+static const char *fonts[]          = {"JetBrainsMono Nerd Font:size=11"};
 static const char dmenufont[]       = "JetBrainsMono Nerd Font:size=12";
 static const char col_gray1[]       = "#282a36";
 static const char col_gray2[]       = "#282a36";
 static const char col_gray3[]       = "#96b5b4";
 static const char col_gray4[]       = "#d7d7d7";
+static const char col_gray5[]       = "#32302f";
 static const char col_cyan[]        = "#282a36";
 static const char col_black[]       = "#000000";
 static const char col_white[]       = "#ffffff";
 static const char col_red[]         = "#eb3449";
 static const char col_yellow[]      = "#ffee75";
-static const char col_green[]       = "#a3be8c";
+static const char col_green[]       = "#1FC167";
 static const char col_orange[]      = "#d08770";
 static const char col_purple[]      = "#b48ead";
 static const char col_pink[]        = "#ffafbc";
 static const char col_blue[]        = "#88c0d0";
+static const char col_gruv_orange[] = "#fe8019";
+
 
 static const char *colors[][3]      = {
 	/*					fg         bg          border   */
-    [SchemeNorm]   = { col_gray3,  col_gray1,  col_gray2 },
-    [SchemeSel]    = { col_gray4,  col_cyan,   col_cyan  },
-    [SchemeRed]    = { col_red,    col_gray1,  col_gray2 },
-    [SchemeYellow] = { col_yellow, col_gray1,  col_gray2 },
-    [SchemeOrange] = { col_orange, col_gray1,  col_gray2 },
-    [SchemeGreen]  = { col_green,  col_gray1,  col_gray2 },
-    [SchemePurple] = { col_purple, col_gray1,  col_gray2 },
-    [SchemePink]   = { col_pink,   col_gray1,  col_gray2 },
-    [Schemeblue]   = { col_blue,   col_gray1,  col_gray2 }
+    [SchemeNorm]   = { col_gruv_orange,  col_gray5,        col_gray2 },
+    [SchemeSel]    = { col_gray5,  		 col_gruv_orange,  col_cyan  },
+    [SchemeRed]    = { col_red,    		 col_gray5,		   col_gray2 },
+    [SchemeYellow] = { col_yellow, 		 col_gray5,  	   col_gray2 },
+    [SchemeOrange] = { col_orange, 		 col_gray5,  	   col_gray2 },
+    [SchemeGreen]  = { col_green,  		 col_gray5,  	   col_gray2 },
+    [SchemePurple] = { col_purple, 		 col_gray5,  	   col_gray2 },
+    [SchemePink]   = { col_pink,   		 col_gray5,  	   col_gray2 },
+    [Schemeblue]   = { col_blue,   		 col_gray5,  	   col_gray2 }
 };
 
-/* staticstatus */
 static const int statmonval = 0;
-
-/* tagging */
 static const char *tags[] = { "I", "II", "III", "IV", "V"};
 
 static const Rule rules[] = {
@@ -53,7 +54,6 @@ static const Rule rules[] = {
 	/* class      instance    title       tags mask     isfloating    centerThisWindow?   monitor */
 	{ "discord",  NULL,       NULL,       1 << 4,       0,            0,                  0 },
 	{ "Spotify",  NULL,       NULL,       1 << 5,       0,            0,                  0 },
-	{ "TelegramDesktop",NULL, NULL,       1 << 6,       0,            0,                  0 },
 };
 
 /* layout(s) */
